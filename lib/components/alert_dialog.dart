afa437017b61fba38922e10f84523151fc037175
Abdulrahman Alkhawwam <abd.kwm@gmail.com>
2024-07-30 00:00:03 +0300

    translate external (1/2)

---
+++ lib/components/alert_dialog.dart
@@ -0,0 +1,237 @@
+import 'dart:io';
+
+import 'package:amity_uikit_beta_service/components/custom_dialog.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/navigation_key.dart';
+
+class AmityDialog {
+  var isShowDialog = true;
+
+  Future<void> showAlertErrorDialog({
+    required String title,
+    required String message,
+  }) async {
+    bool isBarrierDismissible() {
+      return title.toLowerCase().contains("خطأ"); //error
+    }
+
+    if (isShowDialog) {
+      final BuildContext? context =
+          NavigationService.navigatorKey.currentContext;
+      if (context != null) {
+        if (Platform.isIOS) {
+          // Use CupertinoAlertDialog for iOS
+          await showCupertinoDialog(
+            barrierDismissible: isBarrierDismissible(),
+            context: context,
+            builder: (BuildContext context) {
+              return CupertinoAlertDialog(
+                title: Text(title),
+                content: Text(message),
+                actions: <Widget>[
+                  CupertinoDialogAction(
+                    child: const Text('حسنا'), // ok
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        } else {
+          // Use AlertDialog for Android and other platforms
+          await showDialog(
+            barrierDismissible: isBarrierDismissible(),
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text(title),
+                content: Text(message),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('حسنا'), // ok
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        }
+      }
+    }
+  }
+}
+
+class AmityLoadingDialog {
+  static BuildContext? loadingContext;
+  static bool _isDialogShowing = false;
+
+  static Future<void> showLoadingDialog() {
+    print("show AmityLoadingDialog");
+    _isDialogShowing = true;
+    print("set _isDialogShowing: $_isDialogShowing");
+    return showDialog<void>(
+      context: NavigationService.navigatorKey.currentContext!,
+      barrierColor: Colors.transparent,
+      barrierDismissible:
+          true, // Set to false to prevent dismissing by tapping outside
+      builder: (context) {
+        loadingContext = context;
+        return Center(
+          child: SizedBox(
+            width: 200,
+            child: Dialog(
+              backgroundColor: Colors.transparent,
+              elevation: 0, // Remove shadow/elevation effect
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.6),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                padding: const EdgeInsets.all(16.0),
+                child: const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    CupertinoActivityIndicator(
+                      color: Colors.white,
+                      radius: 20,
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      "جاري التحميل", // loading
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    ).then((value) => _isDialogShowing = false);
+  }
+
+  static void hideLoadingDialog() {
+    print("Hide Loading!");
+    print(_isDialogShowing);
+    if (_isDialogShowing) {
+      print("close...");
+      Navigator.of(loadingContext!).pop();
+
+      _isDialogShowing = false;
+    }
+  }
+
+  static Future<void> runWithLoadingDialog<T>(Future<T> Function() task) async {
+    try {
+      showLoadingDialog(); // Show the loading dialog
+      await task(); // Wait for the passed function to complete
+    } catch (e) {
+      // Handle any errors here if needed
+    } finally {
+      hideLoadingDialog(); // Hide the loading dialog
+    }
+  }
+}
+
+class AmitySuccessDialog {
+  static Future<void> showTimedDialog(String text,
+      {BuildContext? context}) async {
+    // if (context == null) {
+    //   print("Context is null, cannot show dialog");
+    //   return Future.value();
+    // }
+
+    showCupertinoDialog<void>(
+      context: context ?? NavigationService.navigatorKey.currentContext!,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return TimedDialog(
+          text: text,
+        );
+      },
+    );
+  }
+}
+
+class ConfirmationDialog {
+  Future<void> show({
+    required BuildContext context,
+    required String title,
+    required String detailText,
+    String leftButtonText = 'إلغاء', //Cancel
+    String rightButtonText = 'تأكيد', //Confirm
+    required Function onConfirm,
+  }) async {
+    // Check the platform
+    if (Platform.isAndroid) {
+      // Android-specific code
+      return showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text(title),
+            content: Text(detailText),
+            actions: <Widget>[
+              TextButton(
+                child: Text(leftButtonText),
+                onPressed: () {
+                  Navigator.of(context).pop(); // Close the dialog
+                },
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  onConfirm();
+                },
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.red, // Set the text color
+                ),
+                child: Text(rightButtonText),
+              ),
+            ],
+          );
+        },
+      );
+    } else if (Platform.isIOS) {
+      // iOS-specific code
+      return showCupertinoDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return CupertinoTheme(
+            data: const CupertinoThemeData(brightness: Brightness.dark),
+            child: CupertinoAlertDialog(
+              title: Text(title),
+              content: Text(detailText),
+              actions: <Widget>[
+                CupertinoDialogAction(
+                  child: Text(leftButtonText),
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the dialog
+                  },
+                ),
+                CupertinoDialogAction(
+                  textStyle: const TextStyle(color: Colors.red),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    onConfirm();
+                  },
+                  isDefaultAction: true,
+                  child: Text(rightButtonText),
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    }
+  }
+}
---
+++ lib/viewmodel/amity_viewmodel.dart
@@ -0,0 +1,106 @@
+import 'dart:developer';
+
+import 'package:amity_sdk/amity_sdk.dart';
+import 'package:flutter/material.dart';
+
+import '../components/alert_dialog.dart';
+
+class AmityVM extends ChangeNotifier {
+  AmityUser? currentamityUser;
+
+  Future<void> login(
+      {required String userID, String? displayName, String? authToken}) async {
+    log("login with $userID");
+    if (authToken == null) {
+      log("authToken == null");
+      if (displayName != null) {
+        await AmityCoreClient.login(userID)
+            .displayName(displayName)
+            .submit()
+            .then((value) async {
+          log("success");
+
+          currentamityUser = value;
+          notifyListeners();
+        }).catchError((error, stackTrace) async {
+          log("error");
+
+          log(error.toString());
+          //        await AmityDialog()
+          //            .showAlertErrorDialog(title: "Error!", message: error.toString());
+        });
+      } else {
+        await AmityCoreClient.login(userID).submit().then((value) async {
+          log("success");
+
+          currentamityUser = value;
+          notifyListeners();
+        }).catchError((error, stackTrace) async {
+          log("error");
+
+          log(error.toString());
+          //        await AmityDialog()
+          //            .showAlertErrorDialog(title: "Error!", message: error.toString());
+        });
+      }
+    } else {
+      log("authToken is provided");
+      if (displayName != null) {
+        log("displayName is provided");
+        await AmityCoreClient.login(userID)
+            .authToken(authToken)
+            .displayName(displayName)
+            .submit()
+            .then((value) async {
+          log("success");
+          print("current amity user :$value");
+          currentamityUser = value;
+          notifyListeners();
+        }).catchError((error, stackTrace) async {
+          log("error");
+
+          log(error.toString());
+          //        await AmityDialog()
+          //            .showAlertErrorDialog(title: "Error!", message: error.toString());
+        });
+      } else {
+        log("displayName is not provided");
+        await AmityCoreClient.login(userID)
+            .authToken(authToken)
+            .submit()
+            .then((value) async {
+          log("success");
+          print("current amity user :$value");
+
+          currentamityUser = value;
+          notifyListeners();
+        }).catchError((error, stackTrace) async {
+          print("error");
+
+          log(error.toString());
+          //        await AmityDialog()
+          //            .showAlertErrorDialog(title: "Error!", message: error.toString());
+        });
+      }
+    }
+  }
+
+  Future<void> refreshCurrentUserData() async {
+    if (currentamityUser != null) {
+      await AmityCoreClient.newUserRepository()
+          .getUser(currentamityUser!.userId!)
+          .then((user) {
+        currentamityUser = user;
+        notifyListeners();
+      }).onError((error, stackTrace) async {
+        log(error.toString());
+        await AmityDialog()
+            .showAlertErrorDialog(title: "Error!", message: error.toString());
+      });
+    }
+  }
+
+  late Function(AmityPost) onShareButtonPressed;
+  void setShareButtonFunction(
+      Function(AmityPost) onShareButtonPressed) {} // Callback function)
+}
